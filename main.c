2543108c71eb0d19fa9ef9ac18044f6b642de372
NiharikaKapoor <122523254+NiharikaKapoor@users.noreply.github.com>
2024-09-05 11:46:38 +0530

    Added information about brackets and variables in code

--- main.c
+++ main.c
@@ -12,12 +12,35 @@ struct CheckResult {
     int printfCount;
     int scanfCount;
     int functionCount;
+    int intCount;
+    int floatCount;
+    int charCount;
+    int doubleCount;
+    int boolCount;
+    int longCount;
+    int stringCount;
+    int bracketErrorLines[MAX_ERRORS];
+    char bracketErrorMessages[MAX_ERRORS][MAX_LINE_LENGTH];
+    int bracketErrorCount;
+};
+
+struct MemoryStruct {
+    char *memory;
+    size_t size;
 };
 
 void initCheckResult(struct CheckResult *result) {
     result->printfCount = 0;
     result->scanfCount = 0;
     result->functionCount = 0;
+    result->intCount = 0;
+    result->floatCount = 0;
+    result->charCount = 0;
+    result->doubleCount = 0;
+    result->boolCount = 0;
+    result->longCount = 0;
+    result->stringCount = 0;
+    result->bracketErrorCount = 0;
 }
 
 int isWordBoundary(char c) {
@@ -41,7 +64,28 @@ int isCommentLine(const char *line) {
     return strstr(line, "//") != NULL;
 }
 
-void processLine(char *line, struct CheckResult *result) {
+void checkBrackets(const char *line, char *bracketStack, int *stackSize, struct CheckResult *result, int lineNumber) {
+    while (*line) {
+        if (*line == '{' || *line == '(' || *line == '[') {
+            bracketStack[(*stackSize)++] = *line;
+        } else if (*line == '}' || *line == ')' || *line == ']') {
+            if (*stackSize == 0 ||
+                (*line == '}' && bracketStack[*stackSize - 1] != '{') ||
+                (*line == ')' && bracketStack[*stackSize - 1] != '(') ||
+                (*line == ']' && bracketStack[*stackSize - 1] != '[')) {
+                snprintf(result->bracketErrorMessages[result->bracketErrorCount], MAX_LINE_LENGTH,
+                         "Mismatched closing bracket: %c", *line);
+                result->bracketErrorLines[result->bracketErrorCount] = lineNumber;
+                result->bracketErrorCount++;
+            } else {
+                (*stackSize)--;
+            }
+        }
+        line++;
+    }
+}
+
+void processLine(char *line, struct CheckResult *result, char *bracketStack, int *stackSize, int lineNumber) {
     char cleanLine[MAX_LINE_LENGTH];
     strncpy(cleanLine, line, MAX_LINE_LENGTH);
 
@@ -52,17 +96,43 @@ void processLine(char *line, struct CheckResult *result) {
         }
     }
 
+    checkBrackets(cleanLine, bracketStack, stackSize, result, lineNumber);
+
     if (isKeyword(cleanLine, "printf")) result->printfCount++;
     if (isKeyword(cleanLine, "scanf")) result->scanfCount++;
     if (isKeyword(cleanLine, "int") && isKeyword(cleanLine, "main")) result->functionCount++; // Count main as int function
+    if (isKeyword(cleanLine, "int") && !isKeyword(cleanLine, "main")) result->intCount++;
+    if (isKeyword(cleanLine, "float")) result->floatCount++;
+    if (isKeyword(cleanLine, "char")) result->charCount++;
+    if (isKeyword(cleanLine, "double")) result->doubleCount++;
+    if (isKeyword(cleanLine, "bool")) result->boolCount++;
+    if (isKeyword(cleanLine, "long")) result->longCount++;
+    if (isKeyword(cleanLine, "std::string")) result->stringCount++;
 }
 
-void displayResult(struct CheckResult *result, int totalLines, int totalFuncs) {
+void handleMultilineComment(FILE *file, char *line) {
+    char *commentStart = strstr(line, "/*");
+    char *commentEnd = strstr(line, "*/");
+
+    if (commentStart != NULL && commentEnd == NULL) {
+        char tempLine[MAX_LINE_LENGTH];
+        while (fgets(tempLine, MAX_LINE_LENGTH, file)) {
+            strncat(line, tempLine, MAX_LINE_LENGTH - strlen(line) - 1);
+            if (strstr(tempLine, "*/")) {
+                break;
+            }
+        }
+    }
+}
+
+
+
+void displayResult(struct CheckResult *result, int totalLines, int totalVars, int totalFuncs) {
     printf("BASIC INFORMATION ABOUT THE ABOVE CODE\n");
     printf("=========================================\n");
     printf("Total num of lines --> %d\n", totalLines);
     printf("Total built in functions used --> %d\n", result->printfCount + result->scanfCount);
-    printf("Total num of functions used --> %d\n\n", totalFuncs);
+    printf("Total num of variables used --> %d\n\n", totalVars);
 
     printf("INFORMATION ABOUT FUNCTIONS USED IN THE ABOVE CODE\n");
     printf("=====================================================\n");
@@ -71,6 +141,18 @@ void displayResult(struct CheckResult *result, int totalLines, int totalFuncs) {
     printf("Total number of \"float\" functions --> %d\n", 0); // Placeholder
     printf("Total number of \"printf\" function --> %d\n", result->printfCount);
     printf("Total number of \"scanf\" function --> %d\n\n", result->scanfCount);
+
+    printf("INFORMATION ABOUT THE BRACKETS USED IN ABOVE CODE\n");
+    printf("====================================================\n");
+    printf("%d closing curly bracket(s) are missing\n", result->bracketErrorCount);
+    printf("Curve brackets are perfectly closed\n"); // Placeholder
+    printf("Long brackets are perfectly closed\n\n"); // Placeholder
+
+    printf("INFORMATION ABOUT THE VARIABLES USED\n");
+    printf("========================================\n");
+    printf("The num of floats variables used are --> %d\n", result->floatCount);
+    printf("The num of int variables used are --> %d\n", result->intCount);
+    printf("The num of char variables used are --> %d\n\n", result->charCount);
 }
 
 void analyzeFile(const char *filename) {
@@ -83,18 +165,31 @@ void analyzeFile(const char *filename) {
     struct CheckResult result;
     initCheckResult(&result);
 
+    char bracketStack[MAX_LINE_LENGTH];
+    int stackSize = 0;
     char line[MAX_LINE_LENGTH];
     int lineNumber = 0;
+    int totalVars = 0;
     int totalFuncs = 0;
 
     while (fgets(line, MAX_LINE_LENGTH, file)) {
         lineNumber++;
-        processLine(line, &result);
+        handleMultilineComment(file, line);
+        processLine(line, &result, bracketStack, &stackSize, lineNumber);
     }
 
+    if (stackSize > 0) {
+        result.bracketErrorLines[result.bracketErrorCount] = lineNumber;
+        snprintf(result.bracketErrorMessages[result.bracketErrorCount], MAX_LINE_LENGTH,
+                 "Mismatched opening bracket(s) found.");
+        result.bracketErrorCount++;
+    }
+
+    totalVars = result.intCount + result.floatCount + result.charCount;
     totalFuncs = result.functionCount;
 
-    displayResult(&result, lineNumber, totalFuncs);
+    displayResult(&result, lineNumber, totalVars, totalFuncs);
+
 
     fclose(file);
 }
@@ -111,5 +206,23 @@ int main(int argc, char *argv[]) {
 
     analyzeFile(filename);
 
+    // Read the entire file content for time complexity analysis
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        printf("Could not open the file: %s\n", filename);
+        return 1;
+    }
+
+    char *code = (char *)malloc(MAX_LINE_LENGTH * MAX_ERRORS);
+    char line[MAX_LINE_LENGTH];
+    code[0] = '\0';
+
+    while (fgets(line, MAX_LINE_LENGTH, file)) {
+        strcat(code, line);
+    }
+    fclose(file);
+
+
+    free(code);
     return 0;
 }
